216024ea7cc92f8804d1309cd0d09fc6a3ff95c8
ArianL20 <94827143+ArianL20@users.noreply.github.com>
2021-11-22 09:01:45 +0100

    Add files via upload
    Du bist dumm

---
+++ Compress2Go/src/Bildanalyse.java
@@ -0,0 +1,9 @@
+
+public class Bildanalyse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Compress2Go/src/Komprimierung.java
@@ -0,0 +1,9 @@
+
+public class Komprimierung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
