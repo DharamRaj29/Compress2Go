a91d80f2d1e14b8ff665561845ff5811c98c24d0
Dharam Raj <d29.raj@gmail.com>
2021-12-06 09:26:38 +0100

    Datei druchgehen

--- Compress2Go/src/Ausfuehren.java
+++ Compress2Go/src/Ausfuehren.java
@@ -1,8 +1,8 @@
 public class Ausfuehren {
     public static void main(String[] args) {
         /*
-        
-        String decomdpressedFile ->Bildpfad
+
+        String uncomdpressedFile ->Bildpfad
         Compress Methode
 
         String compressedFile ->Bildpfad
@@ -10,5 +10,59 @@ public class Ausfuehren {
 
 
         */
+        
+
+    }
+
+   
+
+    public static String compress() {
+        
+        String comp = new String();
+
+        String uncomp = new String();
+        uncomp = "237 28 36 237 28 36 237 28 36 237 28 36 237 28 36";
+
+        for(int i=0; i < uncomp.length(); i++){
+            String tripel = new String();
+
+            String tripel2 = new String();
+
+            //tripel = tripel + uncomp.charAt(i);
+            
+            char leerzeichen = ' ';
+            
+            int leerzeichenZähler = 0;
+             
+            if(uncomp.charAt(i)== leerzeichen){
+                leerzeichenZähler = leerzeichenZähler + 1;
+                if(leerzeichenZähler < 3){
+                    tripel = tripel + uncomp.charAt(i) ;
+                }
+            }
+            else if(leerzeichenZähler == 3){
+                
+                leerzeichenZähler = 0;
+
+                if(uncomp.charAt(i)== leerzeichen){
+                    leerzeichenZähler = leerzeichenZähler + 1;
+                    if(leerzeichenZähler < 3){
+                        tripel2 = tripel2 + uncomp.charAt(i) ;
+                    }
+                }
+
+            }
+            else{
+                if(leerzeichenZähler < 3)
+                tripel = tripel + uncomp.charAt(i);
+            }
+            
+
+
+        }
+
+        
+        return comp;
+        
     }
 }
