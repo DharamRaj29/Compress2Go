7aa7acc1fcb4a085b3871b731da59851f58ccb81
Dharam Raj <d29.raj@gmail.com>
2021-12-06 08:11:59 +0100

    class Ausführen ergänzt

--- Compress2Go/src/Ausfuehren.java
+++ Compress2Go/src/Ausfuehren.java
@@ -1,5 +1,13 @@
 public class Ausfuehren {
     public static void main(String[] args) {
-        
+        /*
+        String decomdpressedFile ->Bildpfad
+        Compress Methode
+
+        String compressedFile ->Bildpfad
+        Decompress Methode
+
+
+        */
     }
 }
