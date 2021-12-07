c110be805ed7812a6cfb1c834d537e52894fff89
Dharam Raj <d29.raj@gmail.com>
2021-12-07 18:03:02 +0100

    Änderungen aus dem Unterricht

--- Compress2Go/src/Ausfuehren.java
+++ Compress2Go/src/Ausfuehren.java
@@ -4,6 +4,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Ausfuehren {
+	
+	static String text = "";
+	
     public static void main(String[] args) {
         /*
 
@@ -17,56 +20,57 @@ public class Ausfuehren {
         */
         Path filePath = Paths.get("H:/", "Desktop", "Informatik", "Kreis.ppm");
     	try {
-			String text = Files.readString(filePath);
+			text = Files.readString(filePath);
 			System.out.println(text);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+    	
+    	
+    	aufteilen(text);
     }
 
    
 
-    public static String compress() {
+    public static String aufteilen(String t) {
         
         String comp = new String();
+        
+        char leerzeichen = ' ';
+        
+        int leerzeichenZaehler = 0;
 
-        String uncomp = new String();
-        uncomp = "237 28 36 237 28 36 237 28 36 237 28 36 237 28 36";
-
-        for(int i=0; i < uncomp.length(); i++){
-            String tripel = new String();
+        for(int i=0; i < t.length(); i++){
+            String tripel1 = new String();
 
             String tripel2 = new String();
 
-            //tripel = tripel + uncomp.charAt(i);
+            tripel1 = tripel1 + t.charAt(i);
             
-            char leerzeichen = ' ';
-            
-            int leerzeichenZähler = 0;
+          
              
-            if(uncomp.charAt(i)== leerzeichen){
-                leerzeichenZähler = leerzeichenZähler + 1;
-                if(leerzeichenZähler < 3){
-                    tripel = tripel + uncomp.charAt(i) ;
+            if(t.charAt(i)== leerzeichen){
+                leerzeichenZaehler = leerzeichenZaehler + 1;
+                if(leerzeichenZaehler < 3){
+                    tripel1 = tripel1 + t.charAt(i) ;
                 }
             }
-            else if(leerzeichenZähler == 3){
+            else if(leerzeichenZaehler == 3){
                 
-                leerzeichenZähler = 0;
+                leerzeichenZaehler = 0;
 
-                if(uncomp.charAt(i)== leerzeichen){
-                    leerzeichenZähler = leerzeichenZähler + 1;
-                    if(leerzeichenZähler < 3){
-                        tripel2 = tripel2 + uncomp.charAt(i) ;
+                if(t.charAt(i)== leerzeichen){
+                    leerzeichenZaehler = leerzeichenZaehler + 1;
+                    if(leerzeichenZaehler < 3){
+                        tripel2 = tripel2 + t.charAt(i) ;
                     }
                 }
 
             }
             else{
-                if(leerzeichenZähler < 3)
-                tripel = tripel + uncomp.charAt(i);
+                if(leerzeichenZaehler < 3)
+                tripel1 = tripel1 + t.charAt(i);
             }
             
 
@@ -78,3 +82,4 @@ public class Ausfuehren {
         
     }
 }
+
