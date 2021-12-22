84b72c445a66a0a31eca30167a4aa95381e6008c
Dharam Raj <d29.raj@gmail.com>
2021-12-22 11:58:23 +0100

    Zusammenführung

--- Compress2Go/src/Ausfuehren.java
+++ Compress2Go/src/Ausfuehren.java
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,16 +8,8 @@ import java.util.Scanner;
 
 public class Ausfuehren {
 	static String text =  "";
-    public static void main(String[] args) {
-        
-		String gesamt = "";
-		
-		/*
-        String uncomdpressedFile ->Bildpfad
-        Compress Methode
-        String compressedFile ->Bildpfad
-        Decompress Methode
-        */
+    public static void main(String[] args) throws IOException {
+     
 
 		// Windows Schule:
         //Path filePath = Paths.get("H:/", "Desktop", "Informatik", "Kreis.ppm");
@@ -28,43 +22,50 @@ public class Ausfuehren {
 		
     	try {
 			text = Files.readString(filePath);
-			//System.out.println(text);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	String text2= "255 255 255 255 255 255 255 255 255 255 255 255 255 255 255";
- 
-		/*
+    	
+		int i = 0;
     	String zeile = "";
-    	System.out.println("/////////////////////////////////////////////////////////////////////////");
     	try (Scanner fileScanner = new Scanner(text)){
 			while(fileScanner.hasNextLine()) {
+			i = i + 1;
+			if(i > 4){
 				zeile = fileScanner.nextLine();
-				//System.out.println(zeile);
 				System.out.println(aufteilen(zeile));
+				}
+				else{
+				System.out.println(fileScanner.nextLine());
+				}
 			}
+    	}
+	//Arian
+	/*
+	try {
+		@SuppressWarnings("resource")
+		FileOutputStream ppm = new FileOutputStream("C:\\Users\\Arian Lösing\\Desktop\\Schule\\Informatik");
+		ppm.write(zeile.getBytes());
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	*/
+
+	//Dharam
+	try {
+		@SuppressWarnings("resource")
+		FileOutputStream ppm = new FileOutputStream("/Users/dharamraj/Documents/Schule/Informatik");
+		ppm.write(zeile.getBytes());
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
-			//gesamt = gesamt + zeile;
-			
 	
-			}
-		*/
+	
+}
 
-		String zeile1 = "";
-		String zeile2 = "";
-		String zeile3 = "";
-		String zeile4 = "";
-		
-
-
-
-		//System.out.println(zeilen(text));
-
-   		//System.out.println(aufteilen(text2));
-
-		System.out.println(umformen(text));
-
-    }
 
    
     
@@ -80,7 +81,6 @@ public class Ausfuehren {
     for(int i = 0; i < t.length(); i++) {
     	if(t.charAt(i) != leerzeichen && i !=  t.length() -1 ) {
     		a = a + t.charAt(i);
-    		//System.out.println(a);
     	}
     	else {
     		if(t.charAt(i) != leerzeichen && i == t.length() - 1) {
@@ -92,14 +92,13 @@ public class Ausfuehren {
     			leerzeichenZaehler = 0;
     	         if(vergleich == "") {
     	        	 vergleich = vergleich + a; 
-    	        	// System.out.println("Vergleich 1 " + vergleich);
+
     	        	 a = "";
     	         }
     	         else {
     	    		vergleich2 = "";
     	    		vergleich2 = a;
     	    		a = "";
-    	    		//System.out.println("vergleich2 " + vergleich2);
     	         }		
     		}
     		else {
@@ -110,7 +109,6 @@ public class Ausfuehren {
     		if(vergleich.equals(vergleich2) && !vergleich2.equals("")) {
        		 counter = counter + 1;
        		 vergleich2 = "";
-       		// System.out.println("counter: " + counter);
        	 }
     		 else {
     			if(vergleich != vergleich2 && !vergleich2.equals("")) {
@@ -133,48 +131,6 @@ public class Ausfuehren {
     return textKomprimiert;
     }
     
-	public  String zeilen(String t) {
-		String zeile1 = "";
-		String zeile2 = "";
-		String zeile3 = "";
-		String zeile4 = "";
-		
-		try (Scanner fileScanner = new Scanner(t)) {
-			zeile1 = fileScanner.nextLine();
-			zeile2 = fileScanner.nextLine();
-			zeile3 = fileScanner.nextLine();
-			zeile4 = fileScanner.nextLine();
-		}
-		
-
-		
-		return zeile1 + zeile2 + zeile3 + zeile4;
-	}
 
 	
-	public static String umformen (String t){
-		String geformt = "";
-		
-		String sampleString = t;
-
-		String zeile1 = "P3";
-		String zeile2 = "# Created by IrfanView";
-		String zeile3 = "661 283";
-		//String zeile4 = "255";
-
-		//System.out.println(sampleString);
-
-		sampleString = sampleString.replace(zeile1, "");
-		sampleString = sampleString.replace(zeile2, "");
-		sampleString = sampleString.replace(zeile3, "");
-		//sampleString = sampleString.replace(zeile4, "");
-
-		System.out.println(sampleString);
-
-
-
-		return geformt;
-	}
-	
-        
-    }
+}
